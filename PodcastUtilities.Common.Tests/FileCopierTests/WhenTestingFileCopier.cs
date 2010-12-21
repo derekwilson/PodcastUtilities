1d1f9600cf48e1c1f9346614cd21a8a03d8d356a
atrevarrow <atrevarrow@users.noreply.github.com>
2010-12-21 23:31:04 +0000

    Added some more (basic) tests for FileCopier

---
+++ PodcastUtilities.Common.Tests/FileCopierTests/WhenTestingFileCopier.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.IO;
+using PodcastUtilities.Common.IO;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.FileCopierTests
+{
+	public abstract class WhenTestingFileCopier
+		: SpecTestBase
+	{
+		protected FileCopier FileCopier { get; set; }
+		
+		protected IDriveInfo DestinationDriveInfo { get; set; }
+		protected IDriveInfoProvider DriveInfoProvider { get; set; }
+		protected IFileUtilities FileUtilities { get; set; }
+
+		protected List<SyncItem> SourceFiles { get; set; }
+		protected List<StatusUpdateEventArgs> StatusUpdates { get; set; }
+
+		protected override void GivenThat()
+		{
+			base.GivenThat();
+
+			DestinationDriveInfo = GenerateMock<IDriveInfo>();
+			DestinationDriveInfo.Stub(i => i.Name).Return("D");
+
+			DriveInfoProvider = GenerateMock<IDriveInfoProvider>();
+			DriveInfoProvider.Stub(p => p.GetDriveInfo(@"d:\Dest"))
+				.Return(DestinationDriveInfo);
+
+			FileUtilities = GenerateMock<IFileUtilities>();
+
+			SourceFiles = new List<SyncItem>
+			              	{
+			              		new SyncItem {Source = new FileInfo(@"c:\Source\A")},
+			              		new SyncItem {Source = new FileInfo(@"c:\Source\B")}
+			              	};
+
+			StatusUpdates = new List<StatusUpdateEventArgs>();
+
+			FileCopier = new FileCopier(DriveInfoProvider, FileUtilities);
+			FileCopier.StatusUpdate += (sender, e) => StatusUpdates.Add(e);
+		}
+
+		protected override void When()
+		{
+			FileCopier.CopyFilesToTarget(SourceFiles, @"c:\Source", @"d:\Dest", 1000, false);
+		}
+	}
+}
