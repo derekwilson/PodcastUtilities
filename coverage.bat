3b552af705c37dc2fda8b8d832c082953776ec90
Derek Wilson <mail@derekwilson.net>
2011-01-31 21:44:36 +0000

    added coverage scripts

---
+++ coverage.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
+"C:\Program Files\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
