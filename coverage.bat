51a955b183e6cc66ed18fb77e80b4d0b3a4adc90
atrevarrow <atrevarrow@localhost>
2011-02-01 23:22:46 +0000

    Fixed partcover scripts/settings to work on win64

--- coverage.bat
+++ coverage.bat
@@ -3,10 +3,14 @@ if not "%ProgramFiles%" == "" goto win32
 echo Cannot find program files environment variable
 pause
 goto end
+
 :win32
 "%ProgramFiles%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
 "%ProgramFiles%\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
+goto end
+
 :win64
-"%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
+"%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.64.xml --output coverage.xml
 "%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
+
 :end
