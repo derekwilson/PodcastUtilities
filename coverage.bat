4a83e6c6ac3bc344f81bd27951f61e89d334636b
Derek Wilson <derekwilson@users.noreply.github.com>
2011-02-01 19:15:32 +0000

    removed pause from script

--- coverage.bat
+++ coverage.bat
@@ -1,6 +1,7 @@
 if not "%PROGRAMFILES(x86)%" == "" goto win64
 if not "%ProgramFiles%" == "" goto win32
 echo Cannot find program files environment variable
+pause
 goto end
 :win32
 "%ProgramFiles%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
@@ -9,4 +10,3 @@ goto end
 "%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
 "%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
 :end
-pause
