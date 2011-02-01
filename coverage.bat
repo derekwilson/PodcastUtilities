676134da340dd1183435057bb49c6abc7645ba34
Derek Wilson <derekwilson@users.noreply.github.com>
2011-02-01 19:10:51 +0000

    removed absolute pathnames from partcover settings file and reworked coverage script so it should work in 64 bit land

--- coverage.bat
+++ coverage.bat
@@ -1,2 +1,12 @@
-"C:\Program Files\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
-"C:\Program Files\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
+if not "%PROGRAMFILES(x86)%" == "" goto win64
+if not "%ProgramFiles%" == "" goto win32
+echo Cannot find program files environment variable
+goto end
+:win32
+"%ProgramFiles%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
+"%ProgramFiles%\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
+:win64
+"%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.exe" --settings PodcastUtilities.Common.Tests.PartcoverSettings.xml --output coverage.xml
+"%PROGRAMFILES(x86)%\PartCover\PartCover .NET 4.0\partcover.browser.exe" --report coverage.xml
+:end
+pause
