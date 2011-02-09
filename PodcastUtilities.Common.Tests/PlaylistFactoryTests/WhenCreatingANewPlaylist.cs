eed80af99928871f9879036b065693563a4afd34
Derek Wilson <mail@derekwilson.net>
2011-02-09 22:23:34 +0000

    Added tests for PlaylistFactory and fixed Partcover report generator script

---
+++ PodcastUtilities.Common.Tests/PlaylistFactoryTests/WhenCreatingANewPlaylist.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PodcastUtilities.Common.Tests.PlaylistFactoryTests
+{
+    public abstract class WhenCreatingANewPlaylist
+        : WhenTestingBehaviour
+    {
+        protected PlaylistFormat PlayListFormat { get; set; }
+        protected IPlaylist Playlist { get; set; }
+        protected IPlaylistFactory PlaylistFactory { get; set; }
+        protected string PlayllistFilename { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+            PlaylistFactory = new PlaylistFactory();
+            PlayllistFilename = "myplaylist.ext";
+        }
+
+        protected override void When()
+        {
+            Playlist = PlaylistFactory.CreatePlaylist(PlayListFormat, PlayllistFilename);
+        }
+    }
+
+    public class WhenCreatingANewAsxPlaylist
+        : WhenCreatingANewPlaylist
+    {
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+            PlayListFormat = PlaylistFormat.ASX;
+        }
+
+        [Test]
+        public void ItShouldReturnTheCorrectObject()
+        {
+            Assert.That(Playlist, Is.InstanceOf<PlaylistAsx>());
+        }
+
+        [Test]
+        public void ItShouldSetTheTitle()
+        {
+            Assert.AreEqual("myplaylist", Playlist.Title);
+        }
+    }
+
+    public class WhenCreatingANewWplPlaylist
+        : WhenCreatingANewPlaylist
+    {
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+            PlayListFormat = PlaylistFormat.WPL;
+        }
+
+        [Test]
+        public void ItShouldReturnTheCorrectObject()
+        {
+            Assert.That(Playlist, Is.InstanceOf<PlaylistWpl>());
+        }
+
+        [Test]
+        public void ItShouldSetTheTitle()
+        {
+            Assert.AreEqual("myplaylist", Playlist.Title);
+        }
+    }
+}
--- coverage_report.bat
+++ coverage_report.bat
@@ -1,4 +1,4 @@
 call GenerateCoverageXml.bat
 rem this is a generator available from http://www.palmmedia.de/Net/ReportGenerator
-"C:\Program Files\ReportGenerator\ReportGenerator.exe" coverage.xml .\Coverage
+"C:\Program Files\PartCover\ReportGenerator\ReportGenerator.exe" coverage.xml .\Coverage
 start .\coverage\index.htm
