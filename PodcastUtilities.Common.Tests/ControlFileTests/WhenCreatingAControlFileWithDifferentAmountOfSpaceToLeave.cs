b6bb85d9fe41541a6b7b822bdd0b459fb3a7e301
Derek Wilson <mail@derekwilson.net>
2011-02-12 16:45:49 +0000

    Added more tests for ControlFile

---
+++ PodcastUtilities.Common.Tests/ControlFileTests/WhenCreatingAControlFileWithDifferentAmountOfSpaceToLeave.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace PodcastUtilities.Common.Tests.ControlFileTests
+{
+    public class WhenCreatingAControlFileWithDifferentAmountOfSpaceToLeave : WhenTestingAControlFile
+    {
+        protected string ControlFileFreeSpaceText { get; set; }
+        protected long FreeSpaceToLeave { get; set; }
+        protected Exception ThrownException { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            XmlNode n = ControlFileXmlDocument.SelectSingleNode("podcasts/global/freeSpaceToLeaveOnDestinationMB");
+            n.InnerText = ControlFileFreeSpaceText;
+
+            ControlFile = new ControlFile(ControlFileXmlDocument);
+        }
+
+        protected override void When()
+        {
+            ThrownException = null;
+            try
+            {
+                FreeSpaceToLeave = ControlFile.FreeSpaceToLeaveOnDestination;
+            }
+            catch (Exception exception)
+            {
+                ThrownException = exception;
+            }
+        }
+    }
+
+    public class WhenCreatingAControlFileWithAnAmountOfSpaceToLeave : WhenCreatingAControlFileWithDifferentAmountOfSpaceToLeave
+    {
+        protected override void GivenThat()
+        {
+            ControlFileFreeSpaceText = "1234";
+            base.GivenThat();
+        }
+
+        [Test]
+        public void ItShouldNotThorw()
+        {
+            Assert.That(ThrownException, Is.Null);
+        }
+
+        [Test]
+        public void ItShouldReadFreeSpaceToLeave()
+        {
+            Assert.That(FreeSpaceToLeave, Is.EqualTo(1234));
+        }
+    }
+
+    public class WhenCreatingAControlFileWithAnIllegalAmountOfSpaceToLeave : WhenCreatingAControlFileWithDifferentAmountOfSpaceToLeave
+    {
+        protected override void GivenThat()
+        {
+            ControlFileFreeSpaceText = "XYZ";
+            base.GivenThat();
+        }
+
+        [Test]
+        public void ItShouldNotThorw()
+        {
+            Assert.That(ThrownException, Is.Null);
+        }
+
+        [Test]
+        public void ItShouldReadFreeSpaceToLeave()
+        {
+            Assert.That(FreeSpaceToLeave, Is.EqualTo(0));
+        }
+    }
+
+}
---
+++ PodcastUtilities.Common.Tests/ControlFileTests/WhenCreatingAnInvalidControlFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace PodcastUtilities.Common.Tests.ControlFileTests
+{
+    public class WhenCreatingAnInvalidControlFile : WhenTestingAControlFile
+    {
+        protected Exception ThrownException { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            XmlNode n = ControlFileXmlDocument.SelectSingleNode("podcasts/global/sourceRoot");
+            n.ParentNode.RemoveChild(n);
+
+            ControlFile = new ControlFile(ControlFileXmlDocument);
+        }
+
+        protected override void When()
+        {
+            ThrownException = null;
+            try
+            {
+                var sourceRoot = ControlFile.SourceRoot;
+            }
+            catch (Exception exception)
+            {
+                ThrownException = exception;
+            }
+        }
+
+        [Test]
+        public void ItShouldThorw()
+        {
+            Assert.That(ThrownException, Is.InstanceOf<Exception>());
+        }
+
+    }
+
+
+}
