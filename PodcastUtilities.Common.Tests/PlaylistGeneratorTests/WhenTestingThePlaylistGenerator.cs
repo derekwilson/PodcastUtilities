74b02caf5f93eb87ba6e8efd7d54c63d332fdb94
Derek Wilson <derekwilson@users.noreply.github.com>
2011-02-15 21:20:46 +0000

    Added first tests for PlaylistGenerator

---
+++ PodcastUtilities.Common.Tests/PlaylistGeneratorTests/WhenTestingThePlaylistGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.PlaylistGeneratorTests
+{
+    public abstract class WhenTestingThePlaylistGenerator
+        : WhenTestingBehaviour
+    {
+        protected PlaylistGenerator PlaylistGenerator { get; set; }
+
+        protected IFileFinder Finder { get; set; }
+        protected IPlaylistFactory Factory { get; set; }
+        protected IPlaylist Playlist { get; set; }
+        protected IControlFile ControlFile { get; set; }
+        protected IList<PodcastInfo> Podcasts { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            Podcasts = new List<PodcastInfo>(10);
+
+            ControlFile = GenerateMock<IControlFile>();
+            ControlFile.Stub(ctrl => ctrl.SourceRoot).Return("c:\\source");
+            ControlFile.Stub(ctrl => ctrl.DestinationRoot).Return("c:\\destination");
+            ControlFile.Stub(ctrl => ctrl.Podcasts).Return(Podcasts);
+
+            Finder = GenerateMock<IFileFinder>();
+
+            Playlist = GenerateMock<IPlaylist>();
+            Factory = GenerateMock<IPlaylistFactory>();
+            Factory.Stub(factory => factory.CreatePlaylist(PlaylistFormat.WPL, null)).IgnoreArguments().Return(Playlist);
+
+            PlaylistGenerator = new PlaylistGenerator(Finder, Factory);
+        }
+    }
+
+}
---
+++ PodcastUtilities.Common.Tests/PlaylistGeneratorTests/WhenThereAreNoSourcePodcasts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.PlaylistGeneratorTests
+{
+    public class WhenThereAreNoSourcePodcasts : WhenTestingThePlaylistGenerator
+    {
+        protected override void When()
+        {
+            PlaylistGenerator.GeneratePlaylist(ControlFile,false);
+        }
+
+        [Test]
+        public void ItShouldSaveTheFile()
+        {
+            Playlist.AssertWasCalled(p => p.SaveFile());
+        }
+
+        [Test]
+        public void ItShouldNotAddTracks()
+        {
+            Playlist.AssertWasNotCalled(p => p.AddTrack(null), o => o.IgnoreArguments());
+        }
+    }
+}
