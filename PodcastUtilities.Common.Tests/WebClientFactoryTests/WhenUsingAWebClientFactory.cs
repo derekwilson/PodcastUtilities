cfd741baf9f0efd0515fbe93d7a8d80884903305
Derek Wilson <mail@derekwilson.net>
2011-05-25 09:06:40 +0200

    Added tests for the webclient factory

---
+++ PodcastUtilities.Common.Tests/WebClientFactoryTests/WhenUsingAWebClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PodcastUtilities.Common.IO;
+
+namespace PodcastUtilities.Common.Tests.WebClientFactoryTests
+{
+    public class WhenUsingAWebClientFactory
+                : WhenTestingBehaviour
+    {
+        private IWebClientFactory _factory;
+        private IWebClient _client;
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+            _factory = new WebClientFactory();
+        }
+
+        protected override void When()
+        {
+            _client = _factory.GetWebClient();
+        }
+
+        [Test]
+        public void ItShouldReturnAWebClient()
+        {
+            Assert.IsInstanceOf(typeof(IWebClient),_client);
+        }
+    }
+}
