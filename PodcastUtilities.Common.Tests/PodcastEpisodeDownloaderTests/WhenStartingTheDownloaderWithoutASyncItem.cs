31546f19b8ac80e5d06d56f923b3363a457189f6
Derek Wilson <derekwilson@users.noreply.github.com>
2011-06-28 21:27:35 +0100

    Tests for the episode downloader task

---
+++ PodcastUtilities.Common.Tests/PodcastEpisodeDownloaderTests/WhenStartingTheDownloaderWithoutASyncItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PodcastUtilities.Common.Tests.PodcastEpisodeDownloaderTests
+{
+    public class WhenStartingTheDownloaderWithoutASyncItem : WhenTestingTheDownloader
+    {
+        protected override void When()
+        {
+            _exception = null;
+            try
+            {
+                _downloader.Start(null);
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
+        }
+
+        [Test]
+        public void ItShouldThrow()
+        {
+            Assert.IsInstanceOf(typeof(DownloaderException),_exception);
+        }
+    }
+}
---
+++ PodcastUtilities.Common.Tests/PodcastEpisodeDownloaderTests/WhenTestingTheDownloader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using PodcastUtilities.Common.Platform;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.PodcastEpisodeDownloaderTests
+{
+    public abstract class WhenTestingTheDownloader
+        : WhenTestingBehaviour
+    {
+        protected PodcastEpisodeDownloader _downloader;
+
+        protected IWebClientFactory _webClientFactory;
+        protected IWebClient _webClient;
+
+        protected IFeedSyncItem _syncItem;
+
+        protected MemoryStream _stream;
+
+        protected System.Exception _exception;
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            _webClientFactory = GenerateMock<IWebClientFactory>();
+            _webClient = GenerateMock<IWebClient>();
+
+            _syncItem = new FeedSyncItem();
+            _exception = null;
+
+            SetupData();
+            SetupStubs();
+
+            _downloader = new PodcastEpisodeDownloader(_webClientFactory);
+        }
+
+        protected virtual void SetupData()
+        {
+            _syncItem.EpisodeUrl = new Uri("http://test");
+            _syncItem.DestinationPath = "c:\\folder\\file.ext";
+            _syncItem.EpisodeTitle = "title";
+        }
+
+        protected virtual void SetupStubs()
+        {
+            _webClient.Stub(client => client.OpenRead(_syncItem.EpisodeUrl)).Return(_stream);
+            _webClientFactory.Stub(factory => factory.GetWebClient()).Return(_webClient);
+        }
+    }
+}
