dce5971355ca3c60b6f9ff7f58d81ae8af40ca29
Derek Wilson <derekwilson@users.noreply.github.com>
2011-06-29 22:09:46 +0100

    Added tests for webclient event mechanism in the episode download task

---
+++ PodcastUtilities.Common.Tests/PodcastEpisodeDownloaderTests/WhenCancellingAStartedDownload.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.PodcastEpisodeDownloaderTests
+{
+    public class WhenCancellingAStartedDownload : WhenTestingTheDownloaderCompletedMechanism
+    {
+        protected override void When()
+        {
+            _downloader.Cancel();
+        }
+
+        [Test]
+        public void ItShouldCancelTheClient()
+        {
+            _webClient.AssertWasCalled(client => client.CancelAsync());
+        }
+    }
+}
---
+++ PodcastUtilities.Common.Tests/PodcastEpisodeDownloaderTests/WhenCancellingTheDownloader.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace PodcastUtilities.Common.Tests.PodcastEpisodeDownloaderTests
+{
+    public class WhenCancellingTheDownloader : WhenTestingTheDownloaderCompletedMechanism
+    {
+        protected override void When()
+        {
+            _webClient.Raise(client => client.DownloadFileCompleted += null, this,
+                             new AsyncCompletedEventArgs(null, true, _syncItem));
+        }
+
+        [Test]
+        public void ItShouldComplete()
+        {
+            Assert.That(_downloader.IsStarted(), Is.True);
+            Assert.That(_downloader.IsComplete(), Is.True);
+        }
+
+        [Test]
+        public void ItShouldSendTheCorrectStatus()
+        {
+            Assert.That(_statusUpdateArgs.Exception, Is.Null);
+            Assert.That(_statusUpdateArgs.MessageLevel, Is.EqualTo(StatusUpdateEventArgs.Level.Status));
+            Assert.That(_statusUpdateArgs.Message, Is.StringContaining("Cancelled"));
+        }
+
+        [Test]
+        public void ItShouldTidyUp()
+        {
+            _webClient.AssertWasCalled(client => client.Dispose());
+        }
+    }
+}
