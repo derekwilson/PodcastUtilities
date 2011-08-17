79588822eed8f0bda5389483b5dc21dabf257f72
atrevarrow <atrevarrow@users.noreply.github.com>
2011-08-17 20:25:16 +0100

    Enable/disable edit podcast command when selection changes

---
+++ PodcastUtilities.Presentation.Tests/ViewModels/ConfigurePodcastsViewModelTests/WhenSelectedPodcastChanges.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using PodcastUtilities.Common;
+using PodcastUtilities.Presentation.ViewModels;
+
+namespace PodcastUtilities.Presentation.Tests.ViewModels.ConfigurePodcastsViewModelTests
+{
+    public class WhenSelectedPodcastChanges
+        : WhenTestingConfigurePodcastsViewModel
+    {
+        protected bool CanExecuteChangedRaised { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            ViewModel.EditPodcastCommand.CanExecuteChanged += 
+                (sender, e) => CanExecuteChangedRaised = true;
+        }
+
+        protected override void When()
+        {
+            ViewModel.SelectedPodcast = new PodcastViewModel(new PodcastInfo());
+        }
+
+        [Test]
+        public void ItShouldRaiseCanExecuteChangedOnEditPodcastCommand()
+        {
+            Assert.That(CanExecuteChangedRaised, Is.True);
+        }
+    }
+}
---
+++ PodcastUtilities.Presentation.Tests/ViewModels/ConfigurePodcastsViewModelTests/WhenSelectedPodcastIsNotNull.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using PodcastUtilities.Common;
+using PodcastUtilities.Presentation.ViewModels;
+
+namespace PodcastUtilities.Presentation.Tests.ViewModels.ConfigurePodcastsViewModelTests
+{
+    public class WhenSelectedPodcastIsNotNull
+        : WhenTestingConfigurePodcastsViewModel
+    {
+        protected bool CanExecuteEdit { get; set; }
+
+        protected override void GivenThat()
+        {
+            base.GivenThat();
+
+            ViewModel.SelectedPodcast = new PodcastViewModel(new PodcastInfo());
+        }
+
+        protected override void When()
+        {
+            CanExecuteEdit = ViewModel.EditPodcastCommand.CanExecute(null);
+        }
+
+        [Test]
+        public void ItShouldBeAbleToExecuteEditCommand()
+        {
+            Assert.That(CanExecuteEdit, Is.True);
+        }
+    }
+}
