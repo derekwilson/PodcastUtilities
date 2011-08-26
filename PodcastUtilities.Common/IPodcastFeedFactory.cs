a48a4364bf969e29ce3893fd1af4a14e1841811f
Derek Wilson <derekwilson@users.noreply.github.com>
2011-08-26 16:56:38 +0100

    Added more tests

--- PodcastUtilities.Common/IPodcastFeedFactory.cs
+++ PodcastUtilities.Common/IPodcastFeedFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,28 +17,5 @@ namespace PodcastUtilities.Common
         /// <param name="feedData">the data for the feed</param>
         /// <returns>a podcast feed object</returns>
         IPodcastFeed CreatePodcastFeed(PodcastFeedFormat playlistFormat, Stream feedData);
-    }
-
-    /// <summary>
-    /// factory to get a feed
-    /// </summary>
-    public class PodcastFeedFactory : IPodcastFeedFactory
-    {
-        /// <summary>
-        /// construct a feed from the supplied data in the specified format
-        /// </summary>
-        /// <param name="playlistFormat">the format of the data</param>
-        /// <param name="feedData">the data for the feed</param>
-        /// <returns>a podcast feed object</returns>
-        public IPodcastFeed CreatePodcastFeed(PodcastFeedFormat playlistFormat, Stream feedData)
-        {
-            switch (playlistFormat)
-            {
-                case PodcastFeedFormat.RSS:
-                    return new PodcastFeedInRssFormat(feedData);
-                default:
-                    throw new ArgumentOutOfRangeException("playlistFormat");
-            }
-        }
     }
 }
