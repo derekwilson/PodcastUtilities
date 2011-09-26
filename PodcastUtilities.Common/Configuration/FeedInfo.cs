cfba722c2cc2873c35e2bb713277c6d2fa851c4f
Derek Wilson <derekwilson@users.noreply.github.com>
2011-09-26 23:03:25 +0100

    Started implementing IXmlSerializable in FeedInfo

--- PodcastUtilities.Common/Configuration/FeedInfo.cs
+++ PodcastUtilities.Common/Configuration/FeedInfo.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using PodcastUtilities.Common.Exceptions;
 
 namespace PodcastUtilities.Common.Configuration
 {    
     /// <summary>
     /// configuration info for a podcast feed
     /// </summary>
-    public class FeedInfo : IFeedInfo
+    public class FeedInfo : IFeedInfo, IXmlSerializable
     {
-        private IControlFileGlobalDefaults _controlFileGlobalDefaults;
+        private readonly IControlFileGlobalDefaults _controlFileGlobalDefaults;
 
         /// <summary>
         /// construct a new feed element
@@ -86,5 +90,104 @@ namespace PodcastUtilities.Common.Configuration
             return copy;
         }
 
+        /// <summary>
+        /// This method is reserved and should not be used. When implementing the IXmlSerializable interface, you should return null (Nothing in Visual Basic) from this method, and instead, if specifying a custom schema is required, apply the <see cref="T:System.Xml.Serialization.XmlSchemaProviderAttribute"/> to the class.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Xml.Schema.XmlSchema"/> that describes the XML representation of the object that is produced by the <see cref="M:System.Xml.Serialization.IXmlSerializable.WriteXml(System.Xml.XmlWriter)"/> method and consumed by the <see cref="M:System.Xml.Serialization.IXmlSerializable.ReadXml(System.Xml.XmlReader)"/> method.
+        /// </returns>
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Generates an object from its XML representation.
+        /// </summary>
+        /// <param name="reader">The <see cref="T:System.Xml.XmlReader"/> stream from which the object is deserialized. 
+        ///                 </param>
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts an object into its XML representation.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Xml.XmlWriter"/> stream to which the object is serialized. 
+        ///                 </param>
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteElementString("url", Address.ToString());
+            if (DownloadStrategy.IsSet)
+            {
+                writer.WriteElementString("downloadStrategy", WriteStrategy(DownloadStrategy));
+            }
+            if (Format.IsSet)
+            {
+                writer.WriteElementString("format", WriteFormat(Format));
+            }
+            if (MaximumDaysOld.IsSet)
+            {
+                writer.WriteElementString("maximumDaysOld", MaximumDaysOld.Value.ToString());
+            }
+            if (NamingStyle.IsSet)
+            {
+                writer.WriteElementString("namingStyle", WriteNamingStyle(NamingStyle));
+            }
+            if (DeleteDownloadsDaysOld.IsSet)
+            {
+                writer.WriteElementString("deleteDownloadsDaysOld", DeleteDownloadsDaysOld.Value.ToString());
+            }
+        }
+
+        private static string WriteNamingStyle(IDefaultableItem<PodcastEpisodeNamingStyle> namingStyle)
+        {
+            switch (namingStyle.Value)
+            {
+                case PodcastEpisodeNamingStyle.UrlFileName:
+                    return "url";
+                case PodcastEpisodeNamingStyle.UrlFileNameAndPublishDateTime:
+                    return "pubdate_url";
+                case PodcastEpisodeNamingStyle.UrlFileNameFeedTitleAndPublishDateTime:
+                    return "pubdate_title_url";
+                case PodcastEpisodeNamingStyle.UrlFileNameFeedTitleAndPublishDateTimeInfolder:
+                    return "pubdate_folder_title_url";
+                case PodcastEpisodeNamingStyle.EpisodeTitle:
+                    return "etitle";
+                case PodcastEpisodeNamingStyle.EpisodeTitleAndPublishDateTime:
+                    return "pubdate_etitle";
+                default:
+                    throw new EnumOutOfRangeException("namingStyle");
+            }
+        }
+
+        private static string WriteStrategy(IDefaultableItem<PodcastEpisodeDownloadStrategy> downloadStrategy)
+        {
+            switch (downloadStrategy.Value)
+            {
+                case PodcastEpisodeDownloadStrategy.All:
+                    return "all";
+                case PodcastEpisodeDownloadStrategy.HighTide:
+                    return "high_tide";
+                case PodcastEpisodeDownloadStrategy.Latest:
+                    return "latest";
+                default:
+                    throw new EnumOutOfRangeException("downloadStrategy");
+            }
+        }
+
+        private static string WriteFormat(IDefaultableItem<PodcastFeedFormat> format)
+        {
+            switch (format.Value)
+            {
+                case PodcastFeedFormat.RSS:
+                    return "rss";
+                case PodcastFeedFormat.ATOM:
+                    return "atom";
+                default:
+                    throw new EnumOutOfRangeException("format");
+            }
+        }
     }
 }
