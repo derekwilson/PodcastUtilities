19a6eb70bb3d89eb17e217a59ad24d61b04d6276
atrevarrow <atrevarrow@users.noreply.github.com>
2011-10-17 20:57:56 +0100

    Finally got defaultable item enum dropdowns bound correctly

--- PodcastUtilities.Presentation/ViewModels/EditPodcastViewModel.cs
+++ PodcastUtilities.Presentation/ViewModels/EditPodcastViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 using PodcastUtilities.Common.Configuration;
 
@@ -23,12 +24,62 @@ namespace PodcastUtilities.Presentation.ViewModels
 
             NamingStyles = CollectionHelper.CreateForDefaultableEnum<PodcastEpisodeNamingStyle>();
 
+            DownloadStrategies = CollectionHelper.CreateForDefaultableEnum<PodcastEpisodeDownloadStrategy>();
+
 			Podcast.PropertyChanged += PodcastPropertyChanged;
 		}
 
 		public PodcastViewModel Podcast { get; private set; }
 
         public IEnumerable<DefaultableValueTypeItem<PodcastEpisodeNamingStyle>> NamingStyles { get; private set; }
+
+        public IEnumerable<DefaultableValueTypeItem<PodcastEpisodeDownloadStrategy>> DownloadStrategies { get; private set; }
+
+        public IDefaultableItem<PodcastEpisodeNamingStyle> SelectedNamingStyle
+        {
+            get
+            {
+                return NamingStyles.First(s => s.Equals(Podcast.Podcast.Feed.NamingStyle));
+            }
+            set
+            {
+                if (!Podcast.Podcast.Feed.NamingStyle.Equals(value))
+                {
+                    if (value.IsSet)
+                    {
+                        Podcast.Podcast.Feed.NamingStyle.Value = value.Value;
+                    }
+                    else
+                    {
+                        Podcast.Podcast.Feed.NamingStyle.RevertToDefault();
+                    }
+                    OnPropertyChanged("SelectedNamingStyle");
+                }
+            }
+        }
+
+        public IDefaultableItem<PodcastEpisodeDownloadStrategy> SelectedDownloadStrategy
+        {
+            get
+            {
+                return DownloadStrategies.First(s => s.Equals(Podcast.Podcast.Feed.DownloadStrategy));
+            }
+            set
+            {
+                if (!Podcast.Podcast.Feed.DownloadStrategy.Equals(value))
+                {
+                    if (value.IsSet)
+                    {
+                        Podcast.Podcast.Feed.DownloadStrategy.Value = value.Value;
+                    }
+                    else
+                    {
+                        Podcast.Podcast.Feed.DownloadStrategy.RevertToDefault();
+                    }
+                    OnPropertyChanged("SelectedDownloadStrategy");
+                }
+            }
+        }
 
 		public ICommand AcceptCommand { get { return _acceptCommand; } }
 
