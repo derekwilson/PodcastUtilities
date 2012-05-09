63d7a728aaf203df1cb19fb0d6b70ffcb19f835e
atrevarrow <atrevarrow@users.noreply.github.com>
2012-05-09 22:33:19 +0100

    More intelligent registering/resolving for ConfigurePodcastsViewModel

--- PodcastUtilities.App/AppIocContainer.cs
+++ PodcastUtilities.App/AppIocContainer.cs
@@ -2,6 +2,7 @@ using PodcastUtilities.App.Services;
 using PodcastUtilities.Common;
 using PodcastUtilities.Ioc;
 using PodcastUtilities.Presentation.Services;
+using PodcastUtilities.Presentation.ViewModels;
 
 namespace PodcastUtilities.App
 {
@@ -18,14 +19,20 @@ namespace PodcastUtilities.App
 			IocRegistration.RegisterPodcastServices(Container);
 
 			RegisterPresentationServices();
+			RegisterViewModels();
 		}
 
-		private static void RegisterPresentationServices()
+	    private static void RegisterPresentationServices()
 		{
 			Container.Register<IApplicationService, ApplicationServiceWpf>();
 			Container.Register<IBrowseForFileService, BrowseForFileServiceWpf>();
 			Container.Register<IDialogService, DialogServiceWpf>();
 			Container.Register<IClipboardService, ClipboardService>();
 		}
-	}
+
+        private static void RegisterViewModels()
+        {
+            Container.Register(typeof(ConfigurePodcastsViewModel));
+        }
+    }
 }
