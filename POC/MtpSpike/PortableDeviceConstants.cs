f93a025214ce3f4158db6579e3ecd0047a659f26
atrevarrow <atrevarrow@users.noreply.github.com>
2013-03-16 10:33:26 +1300

    Added simple POC for playing with MTP functionality

---
+++ POC/MtpSpike/PortableDeviceConstants.cs
@@ -0,0 +1,10 @@
+namespace MtpSpike
+{
+    public static class PortableDeviceConstants
+    {
+        // From PortableDevice.h in the windows SDK
+        public const string WPD_DEVICE_OBJECT_ID = "DEVICE";
+
+        public const int VT_LPWSTR = 31;
+    }
+}
