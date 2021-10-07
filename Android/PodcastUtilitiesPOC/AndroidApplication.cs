4f1838b5c93b2720c4222f6fbee88766836294fe
Derek Wilson <mail@derekwilson.net>
2021-10-07 17:54:45 +1300

    Skeleton app with common and ioc hooked up

---
+++ Android/PodcastUtilitiesPOC/AndroidApplication.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Runtime;
+using Android.Util;
+using PodcastUtilities.Common;
+using PodcastUtilities.Ioc;
+
+namespace PodcastUtilitiesPOC
+{
+    [Application]
+    public class AndroidApplication : Application
+    {
+        public const string APP_NAME = "PodcastUtilities-Tag";
+
+        protected AndroidApplication(System.IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public IIocContainer IocContainer { get; private set; }
+
+        private static IIocContainer InitializeIocContainer()
+        {
+            var container = IocRegistration.GetEmptyContainer();
+
+            IocRegistration.RegisterSystemServices(container);
+            IocRegistration.RegisterPortableDeviceServices(container);
+            IocRegistration.RegisterFileServices(container);
+            IocRegistration.RegisterFeedServices(container);
+            IocRegistration.RegisterPlaylistServices(container);
+
+            return container;
+        }
+
+        public override void OnCreate()
+        {
+            Log.Debug(APP_NAME, "AndroidApplication:OnCreate");
+            base.OnCreate();
+
+            // initialise the IoC container
+            IocContainer = InitializeIocContainer();
+            Log.Debug(APP_NAME, "AndroidApplication:IoC Init");
+        }
+    }
+}
---
+++ Android/PodcastUtilitiesPOC/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using PodcastUtilities.Common.Feeds;
+using PodcastUtilities.Common.Platform;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodcastUtilitiesPOC
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Log.Debug(AndroidApplication.APP_NAME, "MainActivity:OnCreate");
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Get our UI controls from the loaded layout
+            TextView txtView = FindViewById<TextView>(Resource.Id.textView1);
+            List<string> envirnment = WindowsEnvironmentInformationProvider.GetEnvironmentRuntimeDisplayInformation();
+            StringBuilder builder = new StringBuilder();
+            foreach (string line in envirnment)
+            {
+                builder.AppendLine(line);
+            }
+            txtView.Text = builder.ToString();
+
+            IEpisodeFinder podcastEpisodeFinder = null;
+            Log.Debug(AndroidApplication.APP_NAME, $"MainActivity:OnCreate {podcastEpisodeFinder != null}");
+            podcastEpisodeFinder = (Application as AndroidApplication).IocContainer.Resolve<IEpisodeFinder>();
+            Log.Debug(AndroidApplication.APP_NAME, $"MainActivity:OnCreate {podcastEpisodeFinder != null}");
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
