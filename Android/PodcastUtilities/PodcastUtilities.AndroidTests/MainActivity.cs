e54743605032fb61e6bbacc6acaaa7de39c98828
Derek Wilson <mail@derekwilson.net>
2022-02-28 18:07:18 +1300

    Added diagnostic info to unit tests

--- Android/PodcastUtilities/PodcastUtilities.AndroidTests/MainActivity.cs
+++ Android/PodcastUtilities/PodcastUtilities.AndroidTests/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using AndroidX.AppCompat.App;
+using Android.Views;
+using Android.Widget;
+using PodcastUtilities.Common.Platform;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 using Xamarin.Android.NUnitLite;
 
 namespace PodcastUtilities.AndroidTests
@@ -19,6 +22,39 @@ namespace PodcastUtilities.AndroidTests
 
             // Once you called base.OnCreate(), you cannot add more assemblies.
             base.OnCreate(bundle);
+
+            // add in our extra stuff
+            View runButton = FindViewById<View>(Resource.Id.RunTestsButton);
+            if (runButton != null)
+            {
+                var linearLayout = runButton.Parent;
+                if (linearLayout is LinearLayout)
+                {
+                    (linearLayout as LinearLayout).AddView(GenerateDiagnosticInfo());
+                }
+            }
+        }
+
+        private TextView GenerateDiagnosticInfo()
+        {
+            TextView view = new TextView(this);
+            List<string> environment = WindowsEnvironmentInformationProvider.GetEnvironmentRuntimeDisplayInformation();
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(GetVersionDisplay());
+            foreach (string line in environment)
+            {
+                builder.AppendLine(line);
+            }
+            view.Text = builder.ToString();
+
+            view.SetSingleLine(false);
+            return view;
+        }
+
+        private string GetVersionDisplay()
+        {
+            var package = PackageManager.GetPackageInfo(PackageName, 0);
+            return $"v{package.VersionName}, ({package.VersionCode})";
         }
     }
 
