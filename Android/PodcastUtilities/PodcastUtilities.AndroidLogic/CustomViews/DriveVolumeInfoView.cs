ca4f7e99c230caa117b524b9b51d71fef522b264
Derek Wilson <mail@derekwilson.net>
2025-10-14 11:59:48 +1300

    more warnings

--- Android/PodcastUtilities/PodcastUtilities.AndroidLogic/CustomViews/DriveVolumeInfoView.cs
+++ Android/PodcastUtilities/PodcastUtilities.AndroidLogic/CustomViews/DriveVolumeInfoView.cs
@@ -3,7 +3,7 @@ using Android.Text;
 using Android.Text.Style;
 using Android.Util;
 using Android.Views;
-using Android.Widget;
+using PodcastUtilities.AndroidLogic.Utilities;
 
 namespace PodcastUtilities.AndroidLogic.CustomViews
 {
@@ -18,22 +18,26 @@ namespace PodcastUtilities.AndroidLogic.CustomViews
 
     public class DriveVolumeInfoView : LinearLayout, IDriveVolumeInfoView
     {
-        private TextView titleView;
-        private TextView freeSpaceView;
-        private TextView totalSpaceView;
-        private ProgressBar steppedBarView;
+        private struct Controls
+        {
+            public TextView titleView;
+            public TextView freeSpaceView;
+            public TextView totalSpaceView;
+            public ProgressBar steppedBarView;
+        }
+        private Controls viewControls;
 
         public DriveVolumeInfoView(Context context) : base(context)
         {
             Init(context, null, 0);
         }
 
-        public DriveVolumeInfoView(Context context, IAttributeSet attrs) : base(context, attrs)
+        public DriveVolumeInfoView(Context context, IAttributeSet? attrs) : base(context, attrs)
         {
             Init(context, attrs, 0);
         }
 
-        public DriveVolumeInfoView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        public DriveVolumeInfoView(Context context, IAttributeSet? attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
         {
             Init(context, attrs, defStyleAttr);
         }
@@ -42,20 +46,20 @@ namespace PodcastUtilities.AndroidLogic.CustomViews
         {
             get
             {
-                return titleView.Text;
+                return viewControls.titleView.Text ?? "";
             }
             set
             {
-                titleView.Text = value;
+                viewControls.titleView.Text = value;
             }
         }
 
         public void SetSpace(int usedMb, int totalMb, string free, string freeUnits, string total, string totalUnits)
         {
-            steppedBarView.Max = totalMb;
-            steppedBarView.Progress = usedMb;
-            SetValue(freeSpaceView, free, freeUnits);
-            SetValue(totalSpaceView, total, totalUnits);
+            viewControls.steppedBarView.Max = totalMb;
+            viewControls.steppedBarView.Progress = usedMb;
+            SetValue(viewControls.freeSpaceView, free, freeUnits);
+            SetValue(viewControls.totalSpaceView, total, totalUnits);
         }
 
         private void SetValue(TextView view, string value, string unit)
@@ -71,30 +75,30 @@ namespace PodcastUtilities.AndroidLogic.CustomViews
             view.TextFormatted = valueSpan;
         }
 
-        private void Init(Context context, IAttributeSet attrs, int defStyle)
+        private void Init(Context context, IAttributeSet? attrs, int defStyle)
         {
             var view = InflateView(context);
-            titleView = FindViewById<TextView>(Resource.Id.drive_volume_info_title);
-            freeSpaceView = FindViewById<TextView>(Resource.Id.drive_volume_info_free);
-            totalSpaceView = FindViewById<TextView>(Resource.Id.drive_volume_info_total);
-            steppedBarView = FindViewById<ProgressBar>(Resource.Id.drive_volume_stepped_bar);
+            viewControls.titleView = ViewHelper.FindViewByIdOrThrow<TextView>("title", view, Resource.Id.drive_volume_info_title);
+            viewControls.freeSpaceView = ViewHelper.FindViewByIdOrThrow<TextView>("free space", view, Resource.Id.drive_volume_info_free);
+            viewControls.totalSpaceView = ViewHelper.FindViewByIdOrThrow<TextView>("total space", view, Resource.Id.drive_volume_info_total);
+            viewControls.steppedBarView = ViewHelper.FindViewByIdOrThrow<ProgressBar>("stepped bar", view, Resource.Id.drive_volume_stepped_bar);
 
             LoadAttributes(attrs, defStyle);
         }
 
-        private void LoadAttributes(IAttributeSet attrs, int defStyle)
+        private void LoadAttributes(IAttributeSet? attrs, int defStyle)
         {
-            var a = Context.ObtainStyledAttributes(attrs, Resource.Styleable.DriveVolumeInfoView, defStyle, 0);
+            var a = Context?.ObtainStyledAttributes(attrs, Resource.Styleable.DriveVolumeInfoView, defStyle, 0);
 
-            Title = a.GetString(Resource.Styleable.DriveVolumeInfoView_title);
+            Title = a?.GetString(Resource.Styleable.DriveVolumeInfoView_title) ?? "NO TITLE";
 
-            a.Recycle();
+            a?.Recycle();
         }
 
-        private View InflateView(Context context)
+        private View? InflateView(Context context)
         {
-            LayoutInflater inflater = context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
-            return inflater.Inflate(Resource.Layout.view_drive_volume_info, this, true);
+            var inflater = context?.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+            return inflater?.Inflate(Resource.Layout.view_drive_volume_info, this, true);
         }
 
         public View GetView()
